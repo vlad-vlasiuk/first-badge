feb8511952cc3808fef7f253081c91595b078f45
vlad-vlasiuk <vlad.vlasiuk@yahoo.com>
2018-11-28 23:41:08 -0700

    add Spring Boot with Lombok, Web, JPA, MySQL, RabbitMQ

---
+++ src/main/java/com/galvanize/haskellcurryregistration/HaskellCurryRegistrationApplication.java
@@ -0,0 +1,12 @@
+package com.galvanize.haskellcurryregistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaskellCurryRegistrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HaskellCurryRegistrationApplication.class, args);
+    }
+}
